6e63eb2c4138a17fbc1e28ca027d7de070e8f80c
Alex Saber <alexsaber89@gmail.com>
2017-03-28 13:54:25 -0500

    clean up html form;  start js file

--- javascripts/domhandler.js
+++ javascripts/domhandler.js
@@ -1,1 +1,5 @@
-console.log("hello world!");
+function collectUserInput() {
+    let userName = document.getElementById("userName");
+    let userEmail = document.getElementById("userEmail");
+    let userDollarAmount = document.getElementById("userDollarAmount");
+}
