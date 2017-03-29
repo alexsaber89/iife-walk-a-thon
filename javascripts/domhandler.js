e722142c3a4042d6c725a0e99c89240d77d36cfa
Alex Saber <Alex Saber>
2017-03-29 00:15:47 -0500

    add basic logic to collect user input

--- javascripts/domhandler.js
+++ javascripts/domhandler.js
@@ -1,5 +1,36 @@
+let userNameInput = document.getElementById("userName");
+let userEmailInput = document.getElementById("userEmail");
+let userDollarAmountInput = document.getElementById("userDollarAmount");
+let userSubmitBtn = document.getElementById("userSubmitBtn");
+let userClearBtn = document.getElementById("userClearBtn");
+
+userSubmitBtn.addEventListener("click", inputValidation);
+userClearBtn.addEventListener("click", clearUserInput);
+
+function inputValidation() {
+	if ((userNameInput.value) && (userEmailInput.value) && (userDollarAmountInput.value)) {
+		collectUserInput();
+	}
+}
+
 function collectUserInput() {
-    let userName = document.getElementById("userName");
-    let userEmail = document.getElementById("userEmail");
-    let userDollarAmount = document.getElementById("userDollarAmount");
+	let userInputObject = {};
+	let radios = document.getElementsByName('optionsRadios');
+	userInputObject.userName = userNameInput.value;
+	userInputObject.userEmail = userEmailInput.value;
+	userInputObject.userDollarAmount = userDollarAmountInput.value;
+
+	for (let i = 0; i < radios.length; i++) {
+		if (radios[i].checked) {
+			userInputObject.paymentType = radios[i].value;
+		}
+	}
+	console.log("userInputObject: ", userInputObject);
+	clearUserInput();
+}
+
+function clearUserInput() {
+	userName.value = "";
+	userEmail.value = "";
+	userDollarAmount.value = "";
 }
